a6a118b27d205c90b16b13d33a0db4b1ccef2853
jordan <andersonjor@deakin.edu.au>
2024-07-22 21:44:58 +1000

    loading state and spinner for login/register

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Login = () => {
   const { signIn } = UserAuth();
@@ -8,15 +9,21 @@ const Login = () => {
   const navigate = useNavigate();
 
   const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+
+  const [invalidLogin, setInvalidLogin] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true);
       await signIn(loginInfo.email, loginInfo.password);
-
       navigate("/");
     } catch (e) {
       console.log("error");
+      setInvalidLogin(true);
+    } finally {
+      setLoading(false);
     }
 
     setLoginInfo({ email: "", password: "" });
@@ -28,39 +35,48 @@ const Login = () => {
   };
   return (
     <div className="min-h-screen min-w-screen bg-emerald-900 flex justify-center items-center">
-      <div className="min-w-[500px]  flex flex-col gap-4 bg-white rounded-xl p-8">
-        <form
-          className="flex flex-col justify-center w-full gap-4 text-2xl"
-          onSubmit={handleSubmit}
-          action="submit"
-        >
-          <label htmlFor="email">Your email</label>
-          <input
-            value={loginInfo.email}
-            onChange={handleChange}
-            className="p-2 border-2 rounded-lg"
-            type="email"
-            id="email"
-          />
-          <label htmlFor="password">Your Password</label>
-          <input
-            value={loginInfo.password}
-            onChange={handleChange}
-            className="p-2 border-2 rounded-lg"
-            type="password"
-            id="password"
-          />
-          <button
-            className="bg-emerald-900 p-2 mt-2 rounded-xl text-white text-2xl"
-            type="submit"
+      {loading ? (
+        <ClipLoader size={150} color="white" />
+      ) : (
+        <div className="min-w-[500px] flex flex-col items-center justify-center gap-4 bg-white rounded-xl p-8">
+          <form
+            className="flex flex-col justify-center w-full gap-4 text-2xl"
+            onSubmit={handleSubmit}
+            action="submit"
           >
-            Login
-          </button>
-        </form>
-        <Link to="/register" className="text-center text-gray-400 underline">
-          Dont have an Account? Click here
-        </Link>
-      </div>
+            <label htmlFor="email">Your email</label>
+            <input
+              value={loginInfo.email}
+              onChange={handleChange}
+              className="p-2 border-2 rounded-lg"
+              type="email"
+              id="email"
+            />
+            <label htmlFor="password">Your Password</label>
+            <input
+              value={loginInfo.password}
+              onChange={handleChange}
+              className="p-2 border-2 rounded-lg"
+              type="password"
+              id="password"
+            />
+            <button
+              className="bg-emerald-900 p-2 mt-2 rounded-xl text-white text-2xl"
+              type="submit"
+            >
+              Login
+            </button>
+          </form>
+          <Link to="/register" className="text-center text-gray-400 underline">
+            Dont have an Account? Click here
+          </Link>
+          {invalidLogin && (
+            <p className="text-center text-red-500">
+              Incorrect email or password
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext.jsx";
 import { createDocForNewUser } from "../utils/firebase";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Register = () => {
   const { createUser } = UserAuth();
 
   const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
 
   const [contact, setContact] = useState({
     name: "",
@@ -30,11 +33,14 @@ const Register = () => {
       contact.password !== ""
     ) {
       try {
+        setLoading(true);
         const { user } = await createUser(contact.email, contact.password);
         await createDocForNewUser(contact.name, contact.email, user.uid);
         navigate("/");
       } catch (e) {
         alert(e.message);
+      } finally {
+        setLoading(false);
       }
     } else {
       alert("Passwords must match and be at least 6 characters.");
@@ -42,65 +48,69 @@ const Register = () => {
   };
   return (
     <div className="min-h-screen min-w-screen bg-emerald-900 flex justify-center items-center">
-      <div className="min-w-[800px] flex flex-col gap-4 bg-white rounded-xl p-8">
-        <h1 className="text-center text-4xl text-emerald-900 font-bold mb-4">
-          Create a DEV@Deakin Account
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className="w-full flex flex-col gap-6 text-2xl"
-        >
-          <div className="flex">
-            <label htmlFor="name">Name</label>
-            <input
-              value={contact.name}
-              onChange={handleChange}
-              className="w-[450px] ml-auto border-2 p-2 rounded-lg"
-              type="text"
-              id="name"
-            />
-          </div>
-          <div className="flex">
-            <label htmlFor="email">Email</label>
-            <input
-              value={contact.email}
-              onChange={handleChange}
-              className="w-[450px] ml-auto border-2 p-2 rounded-lg"
-              type="email"
-              id="email"
-            />
-          </div>
-          <div className="flex">
-            <label htmlFor="password">Password</label>
-            <input
-              value={contact.password}
-              onChange={handleChange}
-              className="w-[450px] ml-auto border-2 p-2 rounded-lg"
-              type="password"
-              id="password"
-            />
-          </div>
-          <div className="flex">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              value={contact.confirmPassword}
-              onChange={handleChange}
-              className="w-[450px] ml-auto border-2 p-2 rounded-lg"
-              type="password"
-              id="confirmPassword"
-            />
-          </div>
-          <button
-            className="bg-emerald-900 p-2 mt-2 rounded-xl text-white text-2xl"
-            type="submit"
+      {loading ? (
+        <ClipLoader size={150} color="white" />
+      ) : (
+        <div className="min-w-[800px] flex flex-col gap-4 bg-white rounded-xl p-8">
+          <h1 className="text-center text-4xl text-emerald-900 font-bold mb-4">
+            Create a DEV@Deakin Account
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            className="w-full flex flex-col gap-6 text-2xl"
           >
-            Register
-          </button>
-        </form>
-        <Link to="/login" className="text-center text-gray-400 underline">
-          Already have an account? Click here
-        </Link>
-      </div>
+            <div className="flex">
+              <label htmlFor="name">Name</label>
+              <input
+                value={contact.name}
+                onChange={handleChange}
+                className="w-[450px] ml-auto border-2 p-2 rounded-lg"
+                type="text"
+                id="name"
+              />
+            </div>
+            <div className="flex">
+              <label htmlFor="email">Email</label>
+              <input
+                value={contact.email}
+                onChange={handleChange}
+                className="w-[450px] ml-auto border-2 p-2 rounded-lg"
+                type="email"
+                id="email"
+              />
+            </div>
+            <div className="flex">
+              <label htmlFor="password">Password</label>
+              <input
+                value={contact.password}
+                onChange={handleChange}
+                className="w-[450px] ml-auto border-2 p-2 rounded-lg"
+                type="password"
+                id="password"
+              />
+            </div>
+            <div className="flex">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                value={contact.confirmPassword}
+                onChange={handleChange}
+                className="w-[450px] ml-auto border-2 p-2 rounded-lg"
+                type="password"
+                id="confirmPassword"
+              />
+            </div>
+            <button
+              className="bg-emerald-900 p-2 mt-2 rounded-xl text-white text-2xl"
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+          <Link to="/login" className="text-center text-gray-400 underline">
+            Already have an account? Click here
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
